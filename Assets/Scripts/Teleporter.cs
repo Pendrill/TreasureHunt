efc9f9f83680f8faecc9c9ce1ef79de207689f9c
Aleksei Waddington <Aleksei Waddington>
2016-07-20 01:01:55 -0400

    Added Script that enables teleportation. Not as a mechanic, but as a tool for the player to be seemlesly be moved around from one location to the next. It could also be used to trigger the consequences of certain actions. (ie they took the pills so they are moved to the designated area).

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	//public GameObject teleportedObject;
+	public int locationX;
+	public int locationY;
+	public int locationZ;
+
+	void OnTriggerEnter2D (Collider2D player){
+		player.GetComponent<Transform> ().position = new Vector3 (locationX, locationY, locationZ);
+		//player.gameObject.transform.position = new Vector3 (locationX, locationY, locationZ);
+
+	}
+}
