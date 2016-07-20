4e37371c036f511061770a0b3c62069a3809260f
Aleksei Waddington <Aleksei Waddington>
2016-07-20 02:15:00 -0400

    Created a script that can return the x, y, and z coordinate of the specified object.

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+
+	public float getPositionX(){
+		return GetComponent<Transform> ().position.x;
+	}
+	public float getPositionY(){
+		return GetComponent<Transform> ().position.y;
+	}
+	public float getPositionZ(){
+		return GetComponent<Transform> ().position.z;
+	}
+}
