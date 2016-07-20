7f1ffa3a841a17af7d3b84c69386d8d7976abf81
Aleksei Waddington <Aleksei Waddington>
2016-07-20 00:44:15 -0400

    Updated the movement code, so that the character can move faster and the speed vlaue can be changed directly from the Inspector

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	int s = 15000;
+	public int speed = 15000;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,16 +14,16 @@ public class PlayerMovement : MonoBehaviour {
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
 
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			GetComponent<Rigidbody2D> ().velocity += new Vector2 (0, s) * Time.deltaTime;
+			GetComponent<Rigidbody2D> ().velocity += new Vector2 (0, speed) * Time.deltaTime;
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			GetComponent<Rigidbody2D> ().velocity += new Vector2 (-s, 0) * Time.deltaTime;
+			GetComponent<Rigidbody2D> ().velocity += new Vector2 (-speed, 0) * Time.deltaTime;
 		}
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			GetComponent<Rigidbody2D> ().velocity += new Vector2 (s, 0) * Time.deltaTime;
+			GetComponent<Rigidbody2D> ().velocity += new Vector2 (speed, 0) * Time.deltaTime;
 		} 
 		if (Input.GetKey (KeyCode.DownArrow)) {
-			GetComponent<Rigidbody2D> ().velocity += new Vector2 (0, -s) * Time.deltaTime;
+			GetComponent<Rigidbody2D> ().velocity += new Vector2 (0, -speed) * Time.deltaTime;
 		}
 
 	}
