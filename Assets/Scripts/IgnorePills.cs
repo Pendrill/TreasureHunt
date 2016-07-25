a074dd48489ef6bbe5a43bd17d5ec1dcaad98821
Aleksei Waddington <Aleksei Waddington>
2016-07-25 09:23:10 -0400

    Final changes. Bug Fixing

--- Assets/Scripts/IgnorePills.cs
+++ Assets/Scripts/IgnorePills.cs
@@ -4,19 +4,23 @@ using System.Collections;
 public class IgnorePills : MonoBehaviour {
 	public GameObject ThePills;
 	void OnTriggerEnter2D( Collider2D player){
-		if (player.GetComponent<HealthManager> ().getHealth () >= 75) {
-			player.GetComponent<StabilityManager> ().loseStability (5);
-		} else if (player.GetComponent<HealthManager> ().getHealth () >= 50) {
-			player.GetComponent<StabilityManager> ().loseStability (15);
-		} else if (player.GetComponent<HealthManager> ().getHealth () >= 25) {
-			player.GetComponent<StabilityManager> ().loseStability (25);
-		} else {
-			player.GetComponent<StabilityManager> ().loseStability (30);
+		if (player.GetComponent<PlayerMovement> () != null) {
+			if (player.GetComponent<HealthManager> ().getHealth () >= 75) {
+				player.GetComponent<StabilityManager> ().loseStability (5);
+			} else if (player.GetComponent<HealthManager> ().getHealth () >= 50) {
+				player.GetComponent<StabilityManager> ().loseStability (15);
+			} else if (player.GetComponent<HealthManager> ().getHealth () >= 25) {
+				player.GetComponent<StabilityManager> ().loseStability (25);
+			} else {
+				player.GetComponent<StabilityManager> ().loseStability (30);
+			}
+			Destroy (ThePills.gameObject);
 		}
-		Destroy (ThePills.gameObject);
 	}
 	void OnTriggerExit2D(Collider2D player){
-		Destroy (this.gameObject);
+		if (player.GetComponent<PlayerMovement> () != null) {
+			Destroy (this.gameObject);
+		}
 	}
 
 }
