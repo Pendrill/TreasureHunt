a39f08144de26b9d1966f8d913cb527b3dfbb366
Aleksei Waddington <Aleksei Waddington>
2016-07-25 07:17:33 -0400

    Added a working Sound Effect

--- Assets/Scripts/Loren's Code/PlayerMovement.cs
+++ Assets/Scripts/Loren's Code/PlayerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	public int speed = 15000;
 	public char lastKeyPressed;
+	public AudioSource walking;
 	// Use this for initialization
 	void Start () {
 		lastKeyPressed = 'W';
@@ -15,18 +16,30 @@ public class PlayerMovement : MonoBehaviour {
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
 
 		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+			if (!walking.isPlaying) {
+				walking.PlayOneShot (walking.clip);
+			}
 			GetComponent<Rigidbody2D> ().velocity += new Vector2 (0, speed) * Time.deltaTime;
 			lastKeyPressed = 'W';
 		}
 		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
+			if (!walking.isPlaying) {
+				walking.PlayOneShot (walking.clip);
+			}
 			GetComponent<Rigidbody2D> ().velocity += new Vector2 (0, -speed) * Time.deltaTime;
 			lastKeyPressed = 'S';
 		}
 		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+			if (!walking.isPlaying) {
+				walking.PlayOneShot (walking.clip);
+			}
 			GetComponent<Rigidbody2D> ().velocity += new Vector2 (speed, 0) * Time.deltaTime;
 			lastKeyPressed = 'D';
 		} 
 		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+			if (!walking.isPlaying) {
+				walking.PlayOneShot (walking.clip);
+			}
 			GetComponent<Rigidbody2D> ().velocity += new Vector2 (-speed, 0) * Time.deltaTime;
 			lastKeyPressed = 'A';
 		}
