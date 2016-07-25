329aa48aa14df4991a6ec2d1f3b692d68ea6f0cf
Aleksei Waddington <Aleksei Waddington>
2016-07-25 06:30:08 -0400

    5/9 Collisions done. Fixed some art

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D bullet){
+		if (bullet.gameObject.GetComponent<MoveBullet> () != null) {
+			Destroy (bullet.gameObject);
+		}
+	}
+}
